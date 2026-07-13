dbd6c851665f8ce12bf641b3d46a5f6df9a8c6c4
nataliemolendyk <nmolendyk1@babson.edu>
2026-07-13 00:33:19 -0400

    Changed airports

--- js/config.js
+++ js/config.js
@@ -1,14 +1,19 @@
 window.HotelConfig = {
-  AIRPORTS: [
-    { airportCode: "SFO", search: "hotels in San Francisco" },
-    { airportCode: "LAX", search: "hotels in Los Angeles" },
-    { airportCode: "JFK", search: "hotels in New York City" },
-    { airportCode: "ORD", search: "hotels in Chicago" },
-    { airportCode: "ATL", search: "hotels in Atlanta" },
-    { airportCode: "MIA", search: "hotels in Miami" }
-  ],
-
   CHECK_IN: "2026-08-01",
-CHECK_OUT: "2026-08-05"
+  CHECK_OUT: "2026-08-05",
 
+  AIRPORTS: [
+    {
+      airportCode: "SFO",
+      search: "san francisco hotels"
+    },
+    {
+      airportCode: "OAK",
+      search: "oakland hotels"
+    },
+    {
+      airportCode: "SJC",
+      search: "san jose hotels"
+    }
+  ]
 };
