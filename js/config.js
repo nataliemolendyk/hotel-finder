df7b824ff2c3bdc0b0f36b99410eacec9d19cc81
nataliemolendyk <nmolendyk1@babson.edu>
2026-07-12 23:55:07 -0400

    Removed gitignore

---
+++ js/config.js
@@ -0,0 +1,13 @@
+window.HotelConfig = {
+  AIRPORTS: [
+    { airportCode: "SFO", search: "hotels in San Francisco" },
+    { airportCode: "LAX", search: "hotels in Los Angeles" },
+    { airportCode: "JFK", search: "hotels in New York City" },
+    { airportCode: "ORD", search: "hotels in Chicago" },
+    { airportCode: "ATL", search: "hotels in Atlanta" },
+    { airportCode: "MIA", search: "hotels in Miami" }
+  ],
+
+  CHECK_IN: "2025-07-20",
+  CHECK_OUT: "2025-07-23"
+};
