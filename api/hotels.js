18d185a7c6ec40c190b00ce42ea299b6f042c6da
nataliemolendyk <nmolendyk1@babson.edu>
2026-07-13 01:18:05 -0400

    Changed url

--- api/hotels.js
+++ api/hotels.js
@@ -17,6 +17,7 @@ export default async function handler(req, res) {
 
   const url = `https://serpapi.com/hotels.json?${params.toString()}`;
 
+
   try {
     const response = await fetch(url);
     const data = await response.json();
